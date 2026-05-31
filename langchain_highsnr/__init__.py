cc6626b958931a69a0c316c212a31f32fddb20c4
George Skoumas <skoumasgeorge@gmail.com>
2026-05-31 19:06:53 +0300

    fix: bump __version__ to 0.2.0

--- langchain_highsnr/__init__.py
+++ langchain_highsnr/__init__.py
@@ -1,5 +1,5 @@
 from langchain_highsnr.compressors import HighSNRDocumentCompressor
 from langchain_highsnr.transformers import HighSNRDocumentTransformer
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __all__ = ["HighSNRDocumentCompressor", "HighSNRDocumentTransformer"]
